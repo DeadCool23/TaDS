7a2a197d5fdfb2a00bfac0477ac7967d3695dc2c
nisuev <nisuev04@mail.ru>
2023-09-18 01:19:06 +0300

    Reworked logic

--- lab_01/src/errs.c
+++ lab_01/src/errs.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 
 void err_message(err_t err) {
-    if (err == ERR_IO) printf("ERROR: Incorrect input");
-    else if (err == ERR_CALC_RANGE) printf("ERROR: Calculated num out of acceptable values");
-    else printf("ERROR: Undefined");
+    printf("\033[0;31mERROR: \033[0m");
+    if (err == ERR_IO) printf("Incorrect input");
+    else if (err == ERR_CALC_RANGE) printf("Calculated num out of acceptable values");
+    else printf("Undefined");
     printf("\n");
 }
